9156c048c39155418f9da9ba6e39fbc25e8443c5
miguel-stapleton <info@miguelstapleton.art>
2025-08-28 16:13:57 +0100

    wpa 2

--- public/sw.js
+++ public/sw.js
@@ -7,23 +7,25 @@ const STATIC_ASSETS = [
   '/icons/apple-touch-icon-180.png',
 ];
 
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches
-      .open(CACHE)
-      .then((c) => c.addAll(STATIC_ASSETS))
-      .catch((err) => console.warn('[SW] install cache error:', err))
-  );
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    try {
+      const cache = await caches.open(CACHE);
+      await cache.addAll(STATIC_ASSETS);
+    } catch (err) {
+      console.warn('[SW] install cache error:', err);
+    } finally {
+      self.skipWaiting(); // ensure this line exists
+    }
+  })());
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches
-      .keys()
-      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)));
+    await self.clients.claim(); // ensure this line exists
+  })());
 });
 
 self.addEventListener('fetch', (e) => {
